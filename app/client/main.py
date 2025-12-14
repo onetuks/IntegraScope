417d1d7f525a4ed12e196f00421fdd678b0c6179
onetuks <onetuks@naver.com>
2025-12-14 11:32:44 +0900

    feat: #17 configuration, sidebar

--- app/client/main.py
+++ app/client/main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+from app.client.ui import hide_sidebar_nav, render_sidebar_nav
+
 st.set_page_config(page_title="i-Scope", layout="wide")
+
+hide_sidebar_nav()
+render_sidebar_nav()
 
 st.title("i-Scope")
